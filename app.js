00e5f63a8dfce3478640b1ff753d0d7ceb302dab
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2024-10-18 09:55:11 -0600

    Versión 1.1

--- app.js
+++ app.js
@@ -155,5 +155,17 @@ function validarInput() {
     }
 }
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('Service Worker registrado con éxito:', registration);
+            })
+            .catch(error => {
+                console.log('Error al registrar el Service Worker:', error);
+            });
+    });
+}
+
 // Llamar a condiciones iniciales para iniciar el juego
 condicionesIniciales();
